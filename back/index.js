c665074a87697163e950c0678c66dd00b2a482f3
LuiZZZc <75443196+LuiZZZc@users.noreply.github.com>
2024-10-12 18:35:07 -0500

    Update index.js

--- back/index.js
+++ back/index.js
@@ -1,28 +1,13 @@
-const express = require('express');
-const { getConnection } = require("./db/db-connection-mongo");
-const cors = require("cors");
-const app = express();
-const port = 4000;
+const express = require('express')
+const {getConnection } = require("./db/db-connection-mongo")
+const cors = require ("cors")
+const app = express()
+const port = 4000
 
-getConnection();
+getConnection(); 
 
-// Permitir solicitudes desde el frontend local y desde el frontend en Render
-const allowedOrigins = [
-  "http://localhost:3000", // Frontend local
-  "https://front-j7df.onrender.com" // Frontend en Render
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("No permitido por CORS"));
-    }
-  }
-}));
-
-app.use(express.json());
+app.use(cors({origin: "http://localhost:3000"}))
+app.use(express.json())
 
 app.use("/genero", require("./router/genero"));
 app.use("/director", require("./router/director"));
@@ -30,6 +15,9 @@ app.use("/productora", require("./router/productora"));
 app.use("/tipo", require("./router/tipo"));
 app.use("/media", require("./router/media"));
 
+
+
+
 app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
