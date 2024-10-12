3a6c114367a5e2321a58bce9d9dad3402cb775c3
LuiZZZc <75443196+LuiZZZc@users.noreply.github.com>
2024-10-12 18:34:23 -0500

    Update axios-config.js

--- front/src/helper/axios-config.js
+++ front/src/helper/axios-config.js
@@ -1,19 +1,9 @@
 import axios from 'axios';
 
-// Define la URL base dependiendo del entorno
-const baseURL = process.env.NODE_ENV === 'production'
-  ? 'https://s35-act-react-back-front-1.onrender.com/'  // Reemplaza con la URL real de tu backend en Render
-  : 'http://localhost:4000/';  // URL en desarrollo local
-
-// Crea la instancia de Axios
 const axiosInstance = axios.create({
-  baseURL: baseURL,
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  }
+    baseURL: 'http://localhost:4000/'
 });
 
 export {
-  axiosInstance
+    axiosInstance
 }
