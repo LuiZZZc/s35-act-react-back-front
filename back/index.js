e9ce38f7f3f5439dbae2cfc06366f53360e1fece
LuiZZZc <75443196+LuiZZZc@users.noreply.github.com>
2024-10-13 18:16:27 -0500

    Update index.js

--- back/index.js
+++ back/index.js
@@ -6,7 +6,7 @@ const port = 4000
 
 getConnection(); 
 
-app.use(cors({origin: "https://front-j7df.onrender.com/"}))
+app.use(cors({origin: "https://front-j7df.onrender.com"}))
 app.use(express.json())
 
 app.use("/genero", require("./router/genero"));
